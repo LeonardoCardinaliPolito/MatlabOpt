ea12ecce9f661b04dfd22b13db6721d4c9127900
kaanaktas991 <55210265+kaanaktas991@users.noreply.github.com>
2024-03-14 20:22:52 +0100

    trial to merge

--- ML_datacreator_6p_TOIMPROVE2.m
+++ ML_datacreator_6p_TOIMPROVE2.m
@@ -3,6 +3,7 @@ ML_datacreator_6p_TOIMPROVE(save_name)
 
 function [DATA_SET, DATA_SET_mod, TEST_SET, TEST_SET_mod, TARGET_DATA, TARGET_TEST, FILES_LIST_data, FILES_LIST_test] = ML_datacreator_6p_TOIMPROVE(save_name)
 
+parentFolder = 'C:\Users\d058003\OneDrive - Politecnico di Torino\Documenti\GitHub\MatlabOpt';
 total_files_list = getAllFiles(parentFolder, ".s6p");
 
 % find training set files
@@ -35,13 +36,17 @@ TARGET_TEST = zeros(n_test_elements, 1);
 
 %% DATA SET
 for n_file=1:n_training_elements
+for n_file_train=1:n_training_elements
 
+    filePath = FILES_LIST_data{n_file_train};
 
     disp(['Opened file: ' filePath]);
 
     % LABEL
     if contains(filePath, "CSF_H")
+        TARGET_DATA(n_file_train) = 0;
     else
+        TARGET_DATA(n_file_train) = 1;
     end
 
     % processFile(data);
@@ -63,10 +68,12 @@ for n_file=1:n_training_elements
                 Real = real(S_parameter);
                 Imag = imag(S_parameter);
 
+                DATA_SET(2*101*(ind-1) + 1 : 2*101*ind, n_file_train) = [Real, Imag];
 
                 % MODULE
                 Module = abs(S_parameter);
 
+                DATA_SET_mod(101*(ind-1) + 1 : 101*ind, n_file_train) = Module;
 
                 ind = ind+1; %update counter
             end
@@ -75,13 +82,17 @@ for n_file=1:n_training_elements
 end
 
 %% TEST SET
+for n_file_test=1:n_test_elements
 
+    filePath = FILES_LIST_test{n_file_test};
 
     disp(['Opened file: ' filePath]);
 
     % LABEL
     if contains(filePath, "CSF_H")
+        TARGET_TEST(n_file_test) = 0;
     else
+        TARGET_TEST(n_file_test) = 1;
     end
 
     % processFile(data);
@@ -106,10 +117,12 @@ end
                 Real = real(S_parameter);
                 Imag = imag(S_parameter);
 
+                TEST_SET(2*101*(ind-1) + 1 : 2*101*ind, n_file_test) = [Real, Imag];
 
                 % MODULE
                 Module = abs(S_parameter);
 
+                TEST_SET_mod(101*(ind-1) + 1 : 101*ind, n_file_test) = Module;
              
                 ind = ind+1; %update counter
             end
@@ -119,3 +132,4 @@ end
 
 save(strcat(save_name, ".mat"), "DATA_SET", "DATA_SET_mod", "TEST_SET", "TEST_SET_mod", "TARGET_DATA", "TARGET_TEST", "FILES_LIST_data", "FILES_LIST_test");
 
+end 
